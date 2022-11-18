aa5a83150ef89021609011fa715cdfd5c81c992f
vfloria <fvilmot@gmail.com>
2022-11-18 13:00:17 +0100

    test sur app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 let cartes = document.getElementsByClassName("cours_dispo_carte");
+let cartesSelected= document.getElementsByClassName("selected");
 
 Array.from(cartes).forEach((carte) => {
   carte.addEventListener("click", () => {
     carte.classList.toggle("selected");
+
+    console.log(Array.from(cartesSelected).length);
+    
   });
 });
 
-document.title = "blop";
+
+
