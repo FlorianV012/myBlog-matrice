f6408a5b43a96e9d1dd7dd38cac3f14103bc5aca
fvilmot <fvilmot@gmail.com>
2022-11-19 17:44:24 +0100

    Page cours section2 js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 let cartes = document.getElementsByClassName("cours_dispo_carte");
-let cartesSelected= document.getElementsByClassName("selected");
+let cartesSelected = document.getElementsByClassName("selected");
+let nbCartesSelected = document.getElementById("cours_selected");
 
 Array.from(cartes).forEach((carte) => {
   carte.addEventListener("click", () => {
     carte.classList.toggle("selected");
 
-    console.log(Array.from(cartesSelected).length);
-    
+    nbCartesSelected.innerHTML = `<p class="cours_selected">Nombre de cours sélectionnés : ${
+      Array.from(cartesSelected).length
+    }</p>
+    <p class="cours_selected">Supprimer la sélection 🗑</p> `;
+
+    nbCartesSelected.lastElementChild.addEventListener("click", () => {
+      Array.from(cartes).forEach((carte) => {
+        carte.classList.remove("selected");
+      });
+
+      nbCartesSelected.innerHTML = ``;
+    });
   });
 });
-
-
-
