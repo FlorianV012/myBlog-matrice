f656c0b4ada41fd35a873caab515520e89609754
fvilmot <fvilmot@gmail.com>
2022-11-19 18:35:56 +0100

    Page1 Slider

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Page Cours
+
 let cartes = document.getElementsByClassName("cours_dispo_carte");
 let cartesSelected = document.getElementsByClassName("selected");
 let nbCartesSelected = document.getElementById("cours_selected");
@@ -19,4 +21,36 @@ Array.from(cartes).forEach((carte) => {
       nbCartesSelected.innerHTML = ``;
     });
   });
+});
+
+// Slider
+
+let arriere = document.getElementsByClassName("gauche");
+let avant = document.getElementsByClassName("droit");
+let images = document.getElementsByClassName("slider_img");
+
+let imageActive = 0;
+
+arriere[0].addEventListener("click", () => {
+  if (imageActive <= 0) {
+    images[imageActive].classList.remove("active");
+    imageActive = images.length - 1;
+    images[imageActive].classList.add("active");
+  } else {
+    images[imageActive].classList.remove("active");
+    imageActive--;
+    images[imageActive].classList.add("active");
+  }
+});
+
+avant[0].addEventListener("click", () => {
+  if (imageActive >= images.length - 1) {
+    images[imageActive].classList.remove("active");
+    imageActive = 0;
+    images[imageActive].classList.add("active");
+  } else {
+    images[imageActive].classList.remove("active");
+    imageActive++;
+    images[imageActive].classList.add("active");
+  }
 });
